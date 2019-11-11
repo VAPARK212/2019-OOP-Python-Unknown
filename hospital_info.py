084eec1883d6ee5ddeaa3a44b0855f1e9f0e1f53
vapark212 <vapark020212>
2019-11-11 10:28:22 +0900

    changed treatment list

--- hospital_info.py
+++ hospital_info.py
@@ -35,7 +35,6 @@ class Hospital_data:
             Hospitals.update({name_list[j]: ''})
             for i in infolist:
                 Hospitals[name_list[j]] = Hospitals[name_list[j]]+i[j]
-
         return Hospitals
 
 
@@ -43,6 +42,20 @@ hospital1 = Hospital_data()
 list1 = hospital1.get_name_list()
 treatment_list = ['MKioskTy1', 'MKioskTy10', 'MKioskTy11', 'MKioskTy2', 'MKioskTy25', 'MKioskTy3', 'MKioskTy4',
                   'MKioskTy5', 'MKioskTy6', 'MKioskTy7', 'MKioskTy8', 'MKioskTy9']
+"""
+MKioskTy1: 뇌출혈수술
+MKioskTy10: 신생아
+MKioskTy11: 중증화상
+MKioskTy2: 뇌경색의재관류
+MKioskty25: 응급실
+MKioskTy3: 심근경색의재관류
+MKioskTy4: 복부손상의수술
+MKioskTy5: 사지접합의수술
+MKioskTy6: 응급내시경
+MKioskTy7: 응급투석
+MKioskTy8: 조산산모
+MKioskTy9: 정신질환자
+"""
 info1 = []
 for i in treatment_list:
     hospital1.get_info(treatment_name=i, info=info1)
