4906ce2487d4b90f3e13f57aaeef08484521ded2
tony-sj <tonysj@sasa.hs.kr>
2019-11-20 16:11:32 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	hospital_info.py
    #	main_example.py

--- hospital_sort.py
+++ hospital_sort.py
@@ -1,16 +1,17 @@
 import math
 import collections
 from get_location import *
-from hospital_info import *
 
 x = get_local().get_points()[0]
 y = get_local().get_points()[1]
 
-print(x, y)
-
-
+dic_tmp = {}
 
 def sorting(x1, y1):
     Point2D = collections.namedtuple('Point2D', ['x', 'y'])
     p1 = Point2D(x=x1, y=y1)
     p2 = Point2D(x=x2, y=y2)
+
+
+if __name__ == '__main__':
+    print(x, y)
