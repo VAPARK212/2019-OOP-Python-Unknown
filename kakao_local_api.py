045c02f05a6e2c4ac9cc486934b4c625f1731103
tony-sj <tonysj@sasa.hs.kr>
2019-11-20 16:01:54 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	hospital_info.py
    #	main_example.py

--- kakao_local_api.py
+++
@@ -1,9 +0,0 @@
-from kakao_api import *
-
-
-class Kakao_local_api(Kakao_api):
-    def url_maker(self, url):
-        return 'https://dapi.kakao.com' + url
-
-    def local_coord2region(self, input_coord, output_coord, lang):
-        pass
