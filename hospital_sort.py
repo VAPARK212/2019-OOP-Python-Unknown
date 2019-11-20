7066084db92ad64122481eb19e145eeee8ad0fe4
tony-sj <tonysj@sasa.hs.kr>
2019-11-20 15:38:58 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	hospital_info.py
    #	main_example.py

--- hospital_sort.py
+++ hospital_sort.py
@@ -1,1 +1,8 @@
-from kakao_local_api import *
+import math
+import collections
+
+
+def sorting(x1, y1, x2, y2):
+    Point2D = collections.namedtuple('Point2D', ['x', 'y'])
+    p1 = Point2D(x=x1, y=y1)
+    p2 = Point2D(x=x2, y=y2)
