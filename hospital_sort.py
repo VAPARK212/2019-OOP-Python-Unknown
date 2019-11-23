0561674192f43a44b1287749c87a017dfa313d4f
tony-sj <tonysj@sasa.hs.kr>
2019-11-24 01:11:14 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	hospital_info.py
    #	main_example.py

--- hospital_sort.py
+++ hospital_sort.py
@@ -4,21 +4,27 @@ from get_location import *
 
 class Hospital_sort:
     def __init__(self):
-        self.sx = get_local().get_points()[0]
-        self.sy = get_local().get_points()[1]
+        str_sx = get_local().get_points()[0].split('.')
+
+        self.sx = float(get_local().get_points()[0])
+        self.sy = float(get_local().get_points()[1])
         self.dic = {}
 
     def get_distance(self, key, x, y):
         self.dic[key] = math.sqrt((self.sx-x)*(self.sx-x)+(self.sy-y)(self.sy-y))
         return self.dic[key]
 
-    def sorting(self):
+    def sort_by_distance(self):
         sorted_dic = sorted(self.dic.items())
         return sorted_dic
 
 
 if __name__ == "__main__":
-    dic_hos = {'아산병원': {1, 2}, '세종병원': {3, 4}, '신촌세브란스': {5, 6}}
-    dic = Hospital_sort
+    import copy
+
+    dic_hos = {'아산병원': [1, 2], '세종병원': [3, 4], '신촌세브란스': [5, 6]}
+    dic = Hospital_sort()
     for key in dic_hos:
         dic.get_distance(key, dic_hos[key][0], dic_hos[key][1])
+    new_dic = copy.deepcopy(dic.sort_by_distance())
+    print(new_dic)
