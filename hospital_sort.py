37c887b9d657678ca0a74b9e12a58bf214db140e
tony-sj <tonysj@sasa.hs.kr>
2019-11-24 02:13:24 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	hospital_info.py
    #	main_example.py

--- hospital_sort.py
+++ hospital_sort.py
@@ -11,9 +11,14 @@ class Hospital_sort:
     def cal_distance(self, key, x, y):
         self.dic[key] = (self.sx-x)*(self.sx-x)+(self.sy-y)*(self.sy-y)
 
-    def sort_by_distance(self):     # 거리 기준 정렬
+    def sort_by_distance(self):     # 거리 기준 정렬 후 병원 이름 리스트 리턴
         sorted_dic = sorted(self.dic.items(), key=operator.itemgetter(1))
-        return sorted_dic
+        # print(len(sorted_dic))
+        # print(sorted_dic)
+        li = []
+        for i in range(len(sorted_dic)):
+            li.append(sorted_dic[i][0])
+        return li
 
 
 # import하고 사용 예시
@@ -22,5 +27,8 @@ if __name__ == "__main__":
     di = Hospital_sort()    # class
     for keys in dic_hos:
         di.cal_distance(keys, dic_hos[keys][0], dic_hos[keys][1])    # 딕셔너리의 key와 value 값(x좌표 and y좌표)을 cal_distance 함수에 전달
-    new_dic = di.sort_by_distance()     # 반드시 cal_distance 함수 실행 이후에 sort_by_distance 함수를 실행, 거리 기준으로 정렬된 병원 데이터 딕셔너리 리턴
-    print(new_dic)
+    hospital_name_list = di.sort_by_distance()     # 반드시 cal_distance 함수 실행 이후에 sort_by_distance 함수를 실행, 거리 기준으로 정렬된 병원 이름 데이터 리스트 리턴
+    print(hospital_name_list)
+
+    # 제대로 정렬되었는지 확인용
+    # di.sort_by_distance()
